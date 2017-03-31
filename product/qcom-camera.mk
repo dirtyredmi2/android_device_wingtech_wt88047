8b9ad0a4afdb01ef0bdbdc621ec857f644e10f65
pirej <pirej30@yahoo.com>
2017-03-31 07:50:55 +0200

    camera: add camera sensor list needed for libmmcamera2_sensor_modules.so

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -18,4 +18,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.cpp.duplication=false \
     persist.camera.hal.debug.mask=0 \
     media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true
+    media.stagefright.less-secure=true \
+    ro.camera.sensors=ov8865_q8v18a,ov2680_skuhf,ov2680_5987fhq
