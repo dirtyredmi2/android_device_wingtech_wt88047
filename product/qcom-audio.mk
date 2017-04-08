7e09fac66bd20b49ca5905930c9e36e98698e78c
ssawrav <s.sawrav@gmail.com>
2017-04-09 02:29:36 +0530

    Disable fluence

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -23,7 +23,7 @@ PRODUCT_COPY_FILES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.audio.fluencetype=fluence \
-    persist.audio.fluence.voicecall=true \
+    persist.audio.fluence.voicecall=false \
     persist.audio.fluence.voicerec=false \
     persist.audio.fluence.speaker=false \
     av.streaming.offload.enable=true \
