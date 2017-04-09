13cecc87ec585123687cf92c2dbb49d019472a5f
ssawrav <s.sawrav@gmail.com>
2017-04-09 11:00:40 +0530

    Revert "wt88047: Remove F2FS support"
    This reverts commit f028c2e56503ab28daad1216095d4a484d2d283d.

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,5 +1,6 @@
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+TARGET_USERIMAGES_USE_F2FS := true
 BOARD_NO_SECURE_DISCARD := true
 
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_cm
