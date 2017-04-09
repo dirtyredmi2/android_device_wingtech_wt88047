afd67568a4a0f43eaf01f5f8d78272ab9e5a553a
ssawrav <s.sawrav@gmail.com>
2017-04-09 17:23:01 +0530

    Revert "Fix GPS"
    This reverts commit 8060d59c517abda8c82a8282acd3d1e17b6f4e50.

--- gps/loc_api/libloc_api_50001/LocEngAdapter.cpp
+++ gps/loc_api/libloc_api_50001/LocEngAdapter.cpp
@@ -194,11 +194,9 @@ void LocEngAdapter::setXtraUserAgent() {
                 fclose(file);
 
                 // remove trailing spaces
-                char *s;
-                s = buf + strlen(buf);
-                while (--s >= buf) {
-                    if (!isspace(*s)) break;
-                    *s = 0;
+                size_t len = strlen(buf);
+                while (--len >= 0 && isspace(buf[len])) {
+                    buf[len] = '\0';
                 }
             }
 
@@ -377,14 +375,14 @@ void LocEngAdapter::reportPosition(UlpLocation &location,
     }
 }
 
-void LocInternalAdapter::reportSv(GnssSvStatus &svStatus,
+void LocInternalAdapter::reportSv(QcomSvStatus &svStatus,
                                   GpsLocationExtended &locationExtended,
                                   void* svExt){
     sendMsg(new LocEngReportSv(mLocEngAdapter, svStatus,
                                locationExtended, svExt));
 }
 
-void LocEngAdapter::reportSv(GnssSvStatus &svStatus,
+void LocEngAdapter::reportSv(QcomSvStatus &svStatus,
                              GpsLocationExtended &locationExtended,
                              void* svExt)
 {
@@ -534,8 +532,9 @@ enum loc_api_adapter_err LocEngAdapter::setTime(GpsUtcTime time,
     if (mSupportsTimeInjection) {
         LOC_LOGD("%s:%d]: Injecting time", __func__, __LINE__);
         result = mLocApi->setTime(time, timeReference, uncertainty);
+    } else {
+        mSupportsTimeInjection = true;
     }
-
     return result;
 }
 
@@ -565,10 +564,10 @@ enum loc_api_adapter_err LocEngAdapter::setXtraVersionCheck(int check)
     return ret;
 }
 
-void LocEngAdapter::reportGnssMeasurementData(GnssData &gnssMeasurementData)
+void LocEngAdapter::reportGpsMeasurementData(GpsData &gpsMeasurementData)
 {
-    sendMsg(new LocEngReportGnssMeasurement(mOwner,
-                                           gnssMeasurementData));
+    sendMsg(new LocEngReportGpsMeasurement(mOwner,
+                                           gpsMeasurementData));
 }
 
 /*
